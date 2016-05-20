630e53ad9d95d4ce4bf59462cd608ad3b8ef3a6d
jasminvv <jasmin-v-v@hotmail.com>
2016-05-20 12:29:31 +0200

    Added the simple mixture model and plot functions.

--- Functions.R
+++ Functions.R
@@ -1,48 +1,160 @@
-# SIMULATE DATA: Functions: SetDrawProbability and SimulateDataset
+# ***SIMULATE DATA: Functions: SetDrawProbability and SimulateDataset
 
-SetDrawProbability <- function(prior.all) {
+SetDrawProbability <- function(priors) {
   # Function that takes a vector of k priors, and returns a 
   # vector where the priors are added up and divided by the total
   # (newprior2 = prior1+2/total, newprior3 = newprior2+prior3/total, etc)
-  
-  draw.p <- vector(mode = "numeric", length = length(prior.all))
-  
-  for (b in seq_along(prior.all)) {
+  draw.p <- vector(mode = "numeric", length = length(priors))
+  for (b in seq_along(priors)) {
     if (b == 1) {
-      draw.p[b] <- prior.all[b]/sum(prior.all)
+      draw.p[b] <- priors[b]/sum(priors)
     }
     else {
-      draw.p[b] <- draw.p[b - 1] + (prior.all[b] / sum(prior.all))
+      draw.p[b] <- draw.p[b - 1] + (priors[b] / sum(priors))
     }
   }
-  
   return(draw.p)
 }
 
-SimulateDataset <- function(mean.all, sigma.all, prior.all, n) {
+SimulateDataset <- function(means, vars, priors, sample.size) {
   # Function that takes a vector of k means, a vector of k standard deviations,
   # a vector of k priors, and a sample size n. It returns a vector of n simulated 
   # observations taken from a population with k subpopulations (mean, sd and prior from input).
-  
-  if (length(mean.all) != length(sigma.all) || length(mean.all) != length(prior.all) || length(sigma.all) != length(prior.all)) {
+  if (length(means) != length(vars) || length(means) != length(priors) || length(vars) != length(priors)) {
     stop("Not all parameters have been set.")
   }
-  
-  sample.dataset <- vector(mode="numeric", length=n)
-  draw.p <- SetDrawProbability(prior.all)
-  
-  for (i in 1:n) {
+  sample.dataset <- vector(mode="numeric", length=sample.size)
+  draw.p <- SetDrawProbability(priors)
+  for (i in 1:sample.size) {
     random.number = runif(1, min = 0, max = 1)
     for (b in seq_along(draw.p)) {
       if (random.number <= draw.p[b]) {
-        sample.dataset[i] <- rnorm(1, mean.all[b], sigma.all[b])
+        sample.dataset[i] <- rnorm(1, means[b], vars[b])
         break
       }
-      #else {
-      # next
-      #}
     }	  
   }
-  
   return(sample.dataset)
+}
+
+# ***SIMPLE MIXTURE MODEL: Functions:
+
+InitiatePMembership <- function(num.subpopulations, sample.size){
+  # Function that takes the wanted number of subpopulations (k) and sample size (n),
+  # and returns a matrix containing k rows of length n
+  p.membership <- matrix(nrow = num.subpopulations, ncol = sample.size)
+  return(p.membership)
+}
+
+InitiateEstMeans <- function(num.subpopulations, data){
+  # Function that takes the wanted number of subpopulations (k) and the data,
+  # and returns a vector containing k random means between the smallest and 
+  # largest value in the data
+  est.means <- vector(mode = "numeric", length = num.subpopulations)
+  for (b in 1:num.subpopulations) {
+    est.means[b] <- runif(1, min = min(data), max = max(data))
+  }
+  return(est.means)
+}
+
+InitiateEstVars <- function(num.subpopulations, data){
+  # Function that takes the wanted number of subpopulations (k) and the data,
+  # and returns a vector containing k random standard deviations between
+  # 1% and 1/6th of the range of the data
+  est.vars   <- vector(mode = "numeric", length = num.subpopulations)
+  range.data <- max(data) - min(data)
+  for (b in 1:num.subpopulations) {
+    est.vars[b] <- runif(1, min = range.data/100, max = range.data/6)
+  }
+  return(est.vars)
+}
+
+InitiateEstPriors <- function(num.subpopulations){
+  # Function that takes the wanted number of subpopulations (k),
+  # and returns a vector containing k priors that are equal in size (1/k)
+  est.priors <- vector(mode = "numeric", length = num.subpopulations)
+  for (b in 1:num.subpopulations) {
+    est.priors[b] <- 1/num.subpopulations
+  }
+  return(est.priors)
+}
+
+CalculatePMemberships <- function(data, est.vars, est.means, est.priors, p.membership) {
+  # Function that takes the data, and estimation of the prior, mean and 
+  # std.dev of a subpopulation, and the vector containing current probabilities of membership,
+  # and returns one vector of the old and one of the new probabilities that each value 
+  # belongs to any of the subpopulations
+  old.p.membership <- p.membership
+  p.value.given.pop <- vector(mode = "numeric", length = length(est.means))
+  p.times.prior <- vector(mode = "numeric", length = length(est.means))
+  for (x in seq_along(data)) {
+    for (b in seq_along(est.means)) {
+      p.value.given.pop[b] <- (1 / (sqrt(2 * pi * est.vars[b]))) * exp(- (((data[x] - est.means[b]) ^ 2) / (2 * est.vars[b])))
+      p.times.prior[b] <- est.priors[b] * p.value.given.pop[b]
+    }
+    for (b in seq_along(est.means)) {
+      p.membership[b,x] <- p.times.prior[b] / sum(p.times.prior)
+    }
+  }
+  return(p.membership)
+}
+
+CalculateEstMeans <- function(data, p.membership, est.means) {
+  # Function that takes the data, membership probability matrix and 
+  # vector containing the old estimations of the means, and
+  # returns a vector containing the new estimations of the means.
+  rel.weight <- vector(mode = "numeric", length = length(data))
+  for (b in seq_along(est.means)) {
+    for (x in seq_along(data)) {
+      rel.weight[x] <- p.membership[b,x] * data[x]
+    }
+    est.means[b] <- sum(rel.weight) / sum(p.membership[b, ])
+  }
+  return(est.means)
+}
+
+CalculateEstVars <- function(data, p.membership, est.means, est.vars) {
+  # Function that takes the data, membership probability matrix, vector containing the 
+  # estimations of the means, and vector containing the old estimations of the std. devs
+  # and returns a vector containing the new estimations of the std. devs.
+  rel.var <- vector(mode = "numeric", length = length(data))
+  for (b in seq_along(est.vars)) {
+    for (x in seq_along(data)) {
+      rel.var[x] <- p.membership[b,x] * ((data[x] - est.means[b]) ^ 2)
+    }
+    est.vars[b] <- sum(rel.var) / sum(p.membership[b, ])
+  }
+  return(est.vars)
+}
+
+RunMixtureModel <- function(data, num.subpopulations, max.times){
+  n.obs        <- length(data)
+  est.means    <- InitiateEstMeans(num.subpopulations, data)
+  est.vars <- InitiateEstVars(num.subpopulations, data)
+  est.priors   <- InitiateEstPriors(num.subpopulations)
+  p.membership <- InitiatePMembership(num.subpopulations, n.obs)
+  
+  for (n in 1:max.times) {
+    p.membership <- CalculatePMemberships(data, est.vars, est.means, est.priors, p.membership)
+    est.means    <- CalculateEstMeans(data, p.membership, est.means)
+    est.vars <- CalculateEstVars(data, p.membership, est.means, est.vars)
+  }
+  
+  mixture.model <- list("est.means" = est.means, "est.vars" = est.vars, "p.membership" = p.membership)
+  
+  return(mixture.model)
+}
+
+# PLOT RESULTS
+
+PlotResults <- function(data, mixture.model){
+  est.means  <- mixture.model$est.means
+  est.vars <- mixture.model$est.vars
+  p.membership <- mixture.model$p.membership
+  
+  hist(data, breaks = length(data) / 3, xlim = range(data), ylim = NULL, prob = TRUE, xlab = "Data values", main = "Histogram of Data and Density Graphs for Subpopulations")
+  for (b in seq_along(est.means)) {
+    curve(dnorm(x, mean = est.means[b], sd = sqrt(est.vars[b])) / length(est.means), add = TRUE)
+  }
+  #curve(dnorm(x, estmean.all[b], estsigma.all[b]) + ... +  curve(x, estmean.all[k], estsigma.all[k]))
 }
--- TestFunctions.R
+++ TestFunctions.R
@@ -11,3 +11,42 @@ source("Functions.R")
 # data.1 <- SimulateDataset(mean.all, sigma.all, prior.all, 100)
 # mean(data.1)
 
+# InitiateEstMeans(2, c(2,2,2,4,4,4))
+# est.means <- vector(mode = "numeric", length = 2)
+# for (i in 1:2) {
+#   est.means[1] <- runif(1, min = 2, max = 4)
+# }
+
+# InitiateEstSDs(2, c(2,2,2,4,4,4))
+
+# InitiateEstPriors(2)
+
+k.subs <- 2
+means <- c(7, 12)
+vars <- c(1, 2) 
+priors <- c(0.5, 0.5)
+sample.size <- 100
+# # data <- c(1,2,2,2,3,5,34,35,36,40,40,40,45)
+# data <- c(1,2,2,2,3,5,34,35,36,40,40,40,45)
+# est.means <- InitiateEstMeans(k.subs, data)
+# # est.means <- c(2,4)
+# est.std.devs <- InitiateEstSDs(k.subs, data)
+# # est.std.devs <- c(0.33,0.33)
+# est.priors <- InitiateEstPriors(k.subs)
+# p.membership <- InitiatePMembership(k.subs, length(data))
+# old.p.membership <- InitiatePMembership(k.subs, length(data))
+# 
+# 
+# p.membership <- CalculatePMemberships(data, est.std.devs, est.means, est.priors, p.membership)
+# 
+# est.means <- CalculateEstMeanSubpopulation(data, p.membership, est.means)
+# 
+# est.std.devs <- CalculateEstSDSubpopulation(data, p.membership, est.means, est.std.devs)
+
+data <- SimulateDataset(means, vars, priors, sample.size)
+mixture.model <- RunMixtureModel(data, k.subs, 2000)
+PlotResults(data, mixture.model)
+
+
+
+
